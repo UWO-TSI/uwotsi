db56ea2c40730555ace873e856a420e380a85b57
Ehout20 <erichout00@gmail.com>
2025-12-01 00:40:55 -0500

    Update page.tsx

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,11 +1,18 @@
 import HomeHero from "@/components/sections/HomeHero";
 import PathwayCards from "@/components/sections/PathwayCards";
+import NPOHomePage from "@/components/sections/NPO/npo_homePage";
+import NPOAboutProgram from "@/components/sections/NPO/npo_aboutProgram";
+import NPOFormSubmission from "@/components/sections/NPO/npo_formSubmission";
+
 
 export default function HomePage() {
   return (
     <main className="min-h-screen pt-[96px]">
-      <HomeHero />
-      <PathwayCards />
-    </main>
+		<NPOFormSubmission />
+		<NPOAboutProgram />
+		<HomeHero />
+		<NPOHomePage />
+	<PathwayCards />
+</main>
   );
 }
