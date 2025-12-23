c225f830886a0505109df3b7e08613b7c76e3806
Sarah Chiang <schiang0529@gmail.com>
2025-12-23 12:59:38 -0500

    Add CompaniesBuildTimeline to CompaniesPage
    Imported and rendered the CompaniesBuildTimeline component in CompaniesPage to enhance the page content.

--- web/app/Companies/page.tsx
+++ web/app/Companies/page.tsx
@@ -1,11 +1,13 @@
 import CompaniesHero from "./CompaniesHero";
 import CompaniesBuild from "./CompaniesBuild";
+import CompaniesBuildTimeline from "./CompaniesBuildTimeline";
 
 export default function CompaniesPage() {
   return (
     <>
       <CompaniesHero />
       <CompaniesBuild />
+      <CompaniesBuildTimeline />
     </>
   );
 }
